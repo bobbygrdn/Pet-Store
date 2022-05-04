074ea540851c30eba31466e33ecbc0046a1fa8ef
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-04 15:17:22 -0700

    Inplemented the READ and CHANGE methods

--- pets.js
+++ pets.js
@@ -1,6 +1,82 @@
 const input = process.argv;
+const fs = require('fs');
 
-if(input.length < 3) {
+if (input.length < 3) {
     console.log('Usage: node pets.js [read | create | update | destroy]')
-}
+};
 
+readData();
+
+createData();
+
+function readData() {
+
+    if (input[2] === 'read' && input.length === 3) {
+
+        fs.readFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.error(err);
+                return;
+              };
+
+            let info = JSON.parse(data);
+            console.log(info);
+
+        });
+
+    } else if (input[2] === 'read' && input.length > 3) {
+
+        fs.readFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.error(err);
+                return;
+            };
+
+            let info = JSON.parse(data);
+
+            if(input[3] > info.length) {
+                console.log('Usage: node pets.js read INDEX');
+            };
+
+            if(input[3] < 0) {
+                console.log('Usage: node pets.js read INDEX');
+            };
+            
+            console.log(info[input[3]]);
+        });
+    };
+};
+
+function createData () {
+
+    if(input[2] === 'create' && input.length < 6){
+        
+        console.log('Usage: node pets.js create AGE KIND NAME');
+
+    }else if(input[2] === 'create' && input.length === 6) {
+
+        fs.readFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.error(err);
+                return;
+            };
+            let info = JSON.parse(data);
+        
+            let content = {"age":`${input[3]}`, "kind":`${input[4]}`, "name":`${input[5]}`};
+        
+            info.push(content);
+        
+            let newFile = JSON.stringify(info);
+        
+            fs.writeFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', newFile, err => {
+
+                if(err) {
+                    console.log(err);
+                };
+            });
+        });
+    };
+};
