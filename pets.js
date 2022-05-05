2afa14547ced1769f0d580d799e8f9121e5de00a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-05 07:26:53 -0700

    Added the update and destroy feature

--- pets.js
+++ pets.js
@@ -9,6 +9,10 @@ readData();
 
 createData();
 
+updateData();
+
+destroyData();
+
 function readData() {
 
     if (input[2] === 'read' && input.length === 3) {
@@ -74,9 +78,72 @@ function createData () {
             fs.writeFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', newFile, err => {
 
                 if(err) {
-                    console.log(err);
+                    console.error(err);
                 };
             });
         });
     };
 };
+
+function updateData () {
+
+    if (input[2] === 'update' && input.length < 7) {
+
+        console.log('Usage: node pets.js update INDEX AGE KIND NAME');
+
+    }else if (input[2] === 'update' && input.length === 7) {
+
+        fs.readFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.error(err);
+                return;
+            };
+
+            let info = JSON.parse(data);
+
+            let content = {"age":`${input[4]}`, "kind":`${input[5]}`, "name":`${input[6]}`};
+
+            info.splice(input[3], 1, content);
+
+            let newFile = JSON.stringify(info);
+        
+            fs.writeFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', newFile, err => {
+
+                if(err) {
+                    console.error(err);
+                };
+            });
+        });
+    };
+};
+
+function destroyData () {
+    if (input[2] === 'destroy' && input.length < 4) {
+
+        console.log('Usage: node pets.js destroy INDEX');
+
+    } else if (input[2] === 'destroy' && input.length === 4) {
+
+        fs.readFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.error(err);
+                return;
+            };
+
+            let info = JSON.parse(data);
+
+            info.splice(input[3], 1)
+
+            let newFile = JSON.stringify(info);
+        
+            fs.writeFile('/Users/bobby/Desktop/mcsp-12/fs-pet-shop/pets.json', newFile, err => {
+
+                if(err) {
+                    console.error(err);
+                };
+            });
+        });
+    };   
+}
