708f2646f9573439a75ffad9e2b12959ed481026
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-06 13:25:53 -0700

    Updated the functionality of the express server

---
+++ expressServer.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+const fs = require('fs');
+const url = require('url')
+
+app.use(express.json());
+
+app.get('/pets/:id', function(req,res){
+
+    fs.readFile(petsPath, 'utf8', function (err, petsJSON) {
+        if (err) {  
+
+            console.error(err.stack);
+
+        } else {
+
+            var pets = JSON.parse(petsJSON)
+            let index = req.params.id;
+            if (index >= pets.length || index < 0) {
+                res.statusCode = 404
+                res.setHeader('Content-Type', 'text/plain')
+                res.send('Not Found');
+
+            } else {
+                res.json(pets[index]);
+            }
+        }
+    });
+});
+
+app.post('/pets', function(req,res) {
+    res.send("testing");
+    var newObj = req.body;
+
+    fs.readFile(petsPath, 'utf8', function (err, petsJSON){
+        if (err) {
+            console.error(err.stack);
+        } else {
+            var pets = JSON.parse(petsJSON);
+            pets.push(newObj);
+            fs.writeFile(petsPath, JSON.stringify(pets), err => {
+                if (err) {
+                    console.error(err.stack)
+                }
+            })
+        }
+    })
+})
+
+
+
+app.listen(8000,function()
+{
+ console.log('Running')
+});
+
