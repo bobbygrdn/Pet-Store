4083b3273c35c08410ea2fdbbca3ef2366010ff2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-05-17 13:22:32 -0700

    RESTful Express Server Creation

---
+++ restfulExpressServer.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const petsPath = path.join(__dirname, "pets.json");
+const fs = require("fs");
+
+app.use(express.json());
+
+app.post("/pets", (req, res) => {
+  
+    var newObj = req.body;
+  
+    if (newObj.age === '' || newObj.kind === '' || newObj.name === '') {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.send("Bad request");
+    } else {
+      res.send("Added new animal");
+      fs.readFile(petsPath, "utf8", (err, petsJSON) => {
+        if (err) {
+          console.error(err.stack);
+        } else {
+          var pets = JSON.parse(petsJSON);
+          pets.push(newObj);
+  
+          fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
+            if (err) {
+              console.error(err.stack);
+            }
+          });
+        }
+      });
+    }
+  });
+
+app.get("/pets/:id", (req, res) => {
+    fs.readFile(petsPath, "utf8", (err, petsJSON) => {
+    
+            if (err) {
+                console.error(err.stack);
+            } else {
+            var pets = JSON.parse(petsJSON);
+            let index = req.params.id;
+
+                if (index >= pets.length || index < 0) {
+                    res.statusCode = 404;
+                    res.setHeader("Content-Type", "text/plain");
+                    res.send("Not Found");
+                } else {
+                    res.json(pets[index]);
+                };
+            };
+     });
+});
+
+app.patch("/pets/:id", (req,res) => {
+    
+        fs.readFile(petsPath, "utf-8", (err, petsJSON) => {
+            if(err) {
+                console.error(err.stack);
+            } else {
+                res.send("Updated animal");
+                var newObj = req.body;
+                let index = req.params.id;
+                var pets = JSON.parse(petsJSON);
+                var update = pets[index];
+
+                if (update.age !== Number || update.kind === '' || newObj.name === '') {
+                    res.statusCode = 404;
+                    res.setHeader("Content-Type", "text/plain");
+                    res.send("Bad request");
+                } else {
+                    update.name = newObj.name;
+                
+                    fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
+                        if (err) {
+                        console.error(err.stack);
+                        };
+                    });
+                };
+            };
+        });
+});
+
+app.delete("/pets/:id", (req,res) => {
+    
+    fs.readFile(petsPath, "utf-8", (err, petsJSON) => {
+        if(err) {
+            console.error(err.stack);
+        } else {
+            res.send("Deleting animal");
+            let index = req.params.id;
+            var pets = JSON.parse(petsJSON);
+            pets.splice(index, 1)
+            
+            fs.writeFile(petsPath, JSON.stringify(pets), ()=> {
+                if(err) {
+                    console.error(err.stack);
+                };
+            });
+        };
+    });
+});
+
+app.listen(8000, function () {
+  console.log("Running");
+});
